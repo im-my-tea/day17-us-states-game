25e51fd3f7fa40aa0b1c5f7cfdd5c20a1c857343
Ayush Maiti <ayush.maiti39@gmail.com>
2025-12-31 08:41:11 +0530

    Day 17: Completed US States Game with Report Generator

---
+++ main.py
@@ -0,0 +1,38 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+# Load Data
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        missing_states = [state for state in all_states if state not in guessed_states]
+        
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
+    if answer_state in all_states and answer_state not in guessed_states:
+        guessed_states.append(answer_state)
+
+        row = data[data["state"]==answer_state]
+        x = int(row.x.item())
+        y = int(row.y.item())
+
+        tom = turtle.Turtle()
+        tom.hideturtle()
+        tom.pu()
+        tom.goto(x,y)
+        tom.write(answer_state)
+
+screen.mainloop()
